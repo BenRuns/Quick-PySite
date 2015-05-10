4c6b8cfc641aa6a6c5aaad697abdf3fe25ab13d8
BenRuns <benthomaspatterson@gmail.com>
2015-05-09 18:30:20 -0700

    urlib and omdb

--- movie.py
+++ movie.py
@@ -1,23 +1,46 @@
 import re
+import urllib2
+
 class Movie:
-    def __init__(self,title = '',poster_image_url='',trailer_youtube_url = ''):
+    def __init__(self,title = '',
+        poster_image_url='',
+        trailer_youtube_url = '', 
+        imdbID =''):
+
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+        self.imdbID = imdbID
     	self.youtube_id()
+        self.get_omdb_data()
 
     def content_to_html(self):
         return ('''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{self.trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{self.poster_image_url}" width="220" height="342">
-    <h2>{self.title}</h2>
-</div>
-''').format(self = self)
+        <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{self.trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+            <img src="{self.poster_image_url}" width="220" height="342">
+            <h2>{self.title}</h2>
+        </div>
+        ''').format(self = self)
 
     def youtube_id(self):
         youtube_id_match = re.search(r'(?<=v=)[^&#]+', self.trailer_youtube_url)
         youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', self.trailer_youtube_url)
         self.trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
+    
+    def get_omdb_data(self):
+        if self.imdbID:
+            search_url = "http://www.omdbapi.com/?i=" + self.imdbID 
+            response = urllib2.urlopen(search_url)
+            self.omdb_data = response.read()
+        else:
+            print "no imdbID present"
+
+    @classmethod
+    def search_omdb_by_title(self, title):
+        #put a generator here
+        search_url = "http://www.omdbapi.com/?s=" + title
+        response = urllib2.urlopen(search_url)
+        return response.read()
 
 
 
--- test/movie_test.py
+++ test/movie_test.py
@@ -1,8 +1,13 @@
 import unittest
+import mock
 import sys
 sys.path.insert(0, '../')
 from movie import *
 
+# class MovieTestCase(unittest.TestCase):
+# 	def test_movie_create
+b = Movie(title="Frozen",imdbID = "tt2294629")
+print b.content_to_html(), b.omdb_data
+response = Movie.search_omdb_by_title('frozen')
 
-b = Movie()
-print b.content_to_html()
+print response
